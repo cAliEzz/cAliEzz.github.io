9a8bab9d4246a08133479ff5dd37b0c696a2b5be
mainportfolio <94046914+eldiary-git@users.noreply.github.com>
2023-11-14 15:21:55 +0200

    initial commit

---
+++ assets/js/calc.js
@@ -0,0 +1,87 @@
+﻿(function () {
+ 
+  const errors = $('#form-error');
+  function errorMessage(msg) {
+    errors.html(msg);
+    $('#results_div').hide();
+    errors.show();
+    return false;
+  }
+
+  $('#calc-form')?.trigger("reset");
+
+  const calcBtn = document.getElementById('calcBtn');
+  calcBtn.addEventListener('click', e => {
+    const gender = $('input[name="genderRadio"]:checked').val();
+    // const exIntinsity = $('input[name="ExIntinsity"]:checked').val();
+    const exTarget = $('input[name="ExTarget"]:checked').val();
+    const exTimes = $('input[name="exTimes"]:checked').val();
+    // const thyroidGlandProblem = $('#problemsWithGland').is(':checked');
+    const age = $('#AGEINPUT').val();
+    const weight = $('#WEIGHTInput').val();
+    const height = $('#HEIGHTInput').val();
+    
+
+    
+    if(
+      !gender
+      // || !exTarget
+      || !exTimes
+      || age == undefined
+      || weight == undefined
+      || height == undefined
+      ){
+        
+        errorMessage('Some values are wrong, try again please!!')
+        return;
+      }
+    let result = 0;
+    let activeAverage = 0;
+    let AllCals = 0;
+
+    if(gender === 'MALE'){
+      result = (weight * 10) + (height * 6.26) - (age * 5) + 5;
+    }else{
+      result = (weight * 10) + (height * 6.26) - (age * 5) - 161;
+    }
+
+    switch (exTimes) {
+      case '0T':
+        activeAverage = 1.2;
+        break;
+      case '13T':
+        activeAverage = 1.375;
+        break;
+      case '35T':
+        activeAverage = 1.550;
+        break;
+      case '57T':
+        activeAverage = 1.725;
+        break;
+      case '67T':
+        activeAverage = 1.9;
+        break;
+
+      default:
+        break;
+    }
+    
+    AllCals = result * activeAverage;
+   
+    
+    let MildWeight = AllCals - 250;
+    let LossWeight = AllCals - 500;
+      let ExtremeWeight = AllCals - 1000;
+      $('#allCalRes').text('≈ ' + Math.ceil(AllCals) + ' cal/day');
+      $('#LossWeight').html('Weight loss ≈ <span class="text-warning">' + Math.ceil( LossWeight) + '</span> <small>cal/day</small>');
+      $('#MildWeight').html('Mild weight loss ≈ <span class="text-warning">' + Math.ceil(MildWeight) + '</span> <small>cal/day</small>');
+      $('#ExtremeWeight').html('Extreme weight loss ≈ <span class="text-warning"> ' + Math.ceil(ExtremeWeight) + '</span> <small>cal/day</small>');
+   
+
+
+  });
+
+  
+
+
+})();
---
+++ assets/js/main.js
@@ -0,0 +1,53 @@
+/*!
+*  Magdy Elsayyad - Base theme
+* Copyright 2022-2023 Magdy Elsayyad (https://www.linkedin.com/in/magdy-elsayyad-14a086193/)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive a.nav-link:not(.no-script)')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
